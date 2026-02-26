2dcee8631d0aa9278860db9b1a94a0b36b64dae0
Ega Harizky <harizga29@gmail.com>
2026-02-26 16:51:11 +0700

    bootstrap template

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -5,14 +5,14 @@
         <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
         <meta content="Coderthemes" name="author" />
         <!-- App favicon -->
-        <link rel="shortcut icon" href="assets/images/favicon.ico">
+        <link rel="shortcut icon" href="{{ asset('assets') }}/images/favicon.ico">
 
         <!-- App css -->
-        <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
-        <link href="assets/css/app.min.css" rel="stylesheet" type="text/css" id="light-style" />
-        <link href="assets/css/app-dark.min.css" rel="stylesheet" type="text/css" id="dark-style" />
+        <link href="{{ asset('assets') }}/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('assets') }}/css/app.min.css" rel="stylesheet" type="text/css" id="light-style" />
+        <link href="{{ asset('assets') }}/css/app-dark.min.css" rel="stylesheet" type="text/css" id="dark-style" />
 
         <!-- bundle -->
-        <script src="assets/js/vendor.min.js"></script>
-        <script src="assets/js/app.min.js"></script>
+        <script src="{{ asset('assets') }}/js/vendor.min.js"></script>
+        <script src="{{ asset('assets') }}/js/app.min.js"></script>
     </head>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layouts.base');
 });
+
+Route::get('/user/detail', function () {
+    return view('layouts.base');
+});
